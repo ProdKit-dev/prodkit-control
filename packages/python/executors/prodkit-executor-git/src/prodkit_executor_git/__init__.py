a9dc5ce3ac09073c24e850bf66c017b21d3029d8
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-21 02:41:44 +0300

    feat(executor): export Git executor

--- packages/python/executors/prodkit-executor-git/src/prodkit_executor_git/__init__.py
+++ packages/python/executors/prodkit-executor-git/src/prodkit_executor_git/__init__.py
@@ -1,3 +1,6 @@
 """Controlled Git executor boundary."""
 
+from .executor import ConstrainedGitExecutor, GitExecutorConfig
+
+__all__ = ("ConstrainedGitExecutor", "GitExecutorConfig")
 __version__ = "0.0.0"
