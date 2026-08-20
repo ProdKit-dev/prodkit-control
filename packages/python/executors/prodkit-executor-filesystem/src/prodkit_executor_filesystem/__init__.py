41f7d124d1932be699eab469619f7204a34fcc0c
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-21 02:40:20 +0300

    feat(executor): export filesystem executor

--- packages/python/executors/prodkit-executor-filesystem/src/prodkit_executor_filesystem/__init__.py
+++ packages/python/executors/prodkit-executor-filesystem/src/prodkit_executor_filesystem/__init__.py
@@ -1,3 +1,6 @@
 """Controlled filesystem executor boundary."""
 
+from .executor import ConstrainedFilesystemExecutor, FilesystemExecutorConfig
+
+__all__ = ("ConstrainedFilesystemExecutor", "FilesystemExecutorConfig")
 __version__ = "0.0.0"
