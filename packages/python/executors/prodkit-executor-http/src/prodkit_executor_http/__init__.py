fbb19176ab029930ffc8a83ea29042f348695456
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-21 02:40:59 +0300

    feat(executor): export HTTP executor

--- packages/python/executors/prodkit-executor-http/src/prodkit_executor_http/__init__.py
+++ packages/python/executors/prodkit-executor-http/src/prodkit_executor_http/__init__.py
@@ -1,3 +1,6 @@
 """Controlled HTTP executor boundary."""
 
+from .executor import ConstrainedHTTPExecutor, HTTPExecutorConfig
+
+__all__ = ("ConstrainedHTTPExecutor", "HTTPExecutorConfig")
 __version__ = "0.0.0"
