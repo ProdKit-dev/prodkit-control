0b0a8e1f15fbd793f13d91fcf98a923605de3bf0
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-21 02:38:33 +0300

    feat(runtime): export hardened production adapters

--- packages/python/prodkit-control-runtime/src/prodkit_control_runtime/__init__.py
+++ packages/python/prodkit-control-runtime/src/prodkit_control_runtime/__init__.py
@@ -1,11 +1,15 @@
 """Runtime services for the ProdKit control plane."""
 
+from .approvals import HTTPApprovalProvider as HTTPApprovalProvider
+from .artifacts import EncryptedFilesystemArtifactStore as EncryptedFilesystemArtifactStore
+from .attempts import InMemoryExecutionAttemptStore as InMemoryExecutionAttemptStore
 from .broker import ActionBroker as ActionBroker
 from .broker import BrokerOutcome as BrokerOutcome
 from .bundles import EvidenceBundleBuilder as EvidenceBundleBuilder
 from .bundles import EvidenceBundleVerifier as EvidenceBundleVerifier
 from .bundles import evidence_bundle_sha256 as evidence_bundle_sha256
 from .coordinator import RunCoordinator as RunCoordinator
+from .credentials import HTTPCredentialLeaseProvider as HTTPCredentialLeaseProvider
 from .executors import DigestEffectVerifier as DigestEffectVerifier
 from .executors import DryRunExecutor as DryRunExecutor
 from .executors import ExecutorRegistry as ExecutorRegistry
