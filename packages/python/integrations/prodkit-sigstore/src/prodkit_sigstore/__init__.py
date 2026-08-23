b2bad2b767abdf1b3c3084bd065d77f538daf40b
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-23 13:32:08 +0300

    feat(sigstore): export cosign integration

--- packages/python/integrations/prodkit-sigstore/src/prodkit_sigstore/__init__.py
+++ packages/python/integrations/prodkit-sigstore/src/prodkit_sigstore/__init__.py
@@ -1,3 +1,7 @@
-"""Sigstore and in-toto attestation adapter."""
+"""Sigstore signing and verification integration."""
+
+from .cosign import CosignClient as CosignClient
+from .cosign import CosignCommandResult as CosignCommandResult
+from .cosign import SigstoreIntegrationError as SigstoreIntegrationError
 
 __version__ = "0.2.0"
