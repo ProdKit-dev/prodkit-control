27c74c059ba13c550e507452a6e61646284363a3
Ehsan Azari <im.ehsan.azari@gmail.com>
2026-08-25 21:49:20 +0300

    fix: restore core public exports

--- packages/python/prodkit-control-core/src/prodkit_control_core/__init__.py
+++ packages/python/prodkit-control-core/src/prodkit_control_core/__init__.py
@@ -1,3 +1,10 @@
-"""ProdKit Control canonical models and contracts."""
+"""Canonical contracts for the ProdKit control plane."""
+
+from .canonical import canonical_json_bytes as canonical_json_bytes
+from .canonical import sha256_hex as sha256_hex
+from .contracts import *
+from .exceptions import *
+from .ports import *
+from .reconciliation import *
 
 __version__ = "0.9.0"
